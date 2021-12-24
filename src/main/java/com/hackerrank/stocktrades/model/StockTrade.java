6b3746f4db5ef30d86bc00ad8c4c38408dbdcc21
Git Server Admin <admin@git-server>
2021-12-24 06:10:33 +0000

    Add initial repository

---
+++ src/main/java/com/hackerrank/stocktrades/model/StockTrade.java
@@ -0,0 +1,28 @@
+package com.hackerrank.stocktrades.model;
+
+public class StockTrade {
+    private Integer id;
+    private String type;
+    private Integer userId;
+    private String symbol;
+    private Integer shares;
+    private Integer price;
+    private Long timestamp;
+
+    public StockTrade(String type, Integer userId, String symbol, Integer shares, Integer price, Long timestamp) {
+    }
+
+    public Integer getId() {
+        return null;
+    }
+
+    public String getType() {
+        return null;
+    }
+
+    public void setType(String type) {
+    }
+
+    public void setShares(int shares) {
+    }
+}
